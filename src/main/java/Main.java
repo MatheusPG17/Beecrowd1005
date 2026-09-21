ffa05e49af5d7c2ab64010917fd9e9748a01f177
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-21 15:11:52 -0300

    add comentários

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        
+        //Variaveis
         double notaA, notaB, MEDIA;
         
+        //entrada de dados
         notaA = leia.nextDouble();
         notaB = leia.nextDouble();
         
+        //processamento
         MEDIA = ((notaA * 3.5) + (notaB * 7.5)) / 11;
         
+        //saida de dados
         System.out.printf("MEDIA = %.5f\n", MEDIA);
     }
 }
